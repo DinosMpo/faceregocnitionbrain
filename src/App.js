3c8d9deae006c530d4be5574020da23cb13f93e4
DinosMpo <dinos7591@yahoo.gr>
2022-07-28 12:29:31 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3001/imageurl', {
+      fetch('http://postgresql-horizontal-24282/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -94,7 +94,7 @@ class App extends Component {
       .then(response => {
         console.log('hi', response)
         if (response) {
-          fetch('http://localhost:3001/image', {
+          fetch('http://postgresql-horizontal-24282/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
